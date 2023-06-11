a2ece85330ba8d45de2b38ce723687aaeae0c891
Masagus Hariadi Arief <adi.arief77@gmail.com>
2023-06-11 23:03:10 +0700

    cp

--- src/index.js
+++ src/index.js
@@ -14,21 +14,21 @@ import './Style/Main.scss'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <>
-        {/* <React.StrictMode> */}
-        <App />
-        <ToastContainer
-            position="top-right"
-            autoClose={5000}
-            hideProgressBar={false}
-            newestOnTop={false}
-            closeOnClick
-            rtl={false}
-            pauseOnFocusLoss
-            draggable
-            pauseOnHover
-            theme="light" />
+        <React.StrictMode>
+            <App />
+            <ToastContainer
+                position="top-right"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="light" />
 
-        {/* </React.StrictMode > */}
+        </React.StrictMode >
     </>
 );
 
